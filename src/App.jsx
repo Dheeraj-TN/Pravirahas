ac979cf01ab07ad6199e382bbb7e5110adeba4fc
Dheeraj-TN <dheerajtn394@gmail.com>
2023-10-29 13:41:29 +0530

    header commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
-      <Footer/>
     </div>
   );
 }
